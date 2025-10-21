54380458be58d6cefc6daa8cd6d05481bd911774
8dmusicbalochi <8dmusicbalochi@gmail.com>
2025-10-21 13:24:10 +0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,10 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://aalssohunyxerncystdm.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhbHNzb2h1bnl4ZXJuY3lzdGRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NDkyOTUsImV4cCI6MjA3NjUyNTI5NX0.-SWGrWQyeRILak9QKweM6PHyyiXIxXWhbdeTrrottCg";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL and Anon Key must be provided.');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
